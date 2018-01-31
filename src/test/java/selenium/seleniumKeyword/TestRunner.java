46fdd5163baef087deed74b6e1815d78f5e3abba
TrungT <totrung@gmail.com>
2018-01-31 20:38:22 +0700

    update library

--- src/test/java/selenium/seleniumKeyword/TestRunner.java
+++ src/test/java/selenium/seleniumKeyword/TestRunner.java
@@ -1,11 +1,9 @@
 package selenium.seleniumKeyword;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
-import io.appium.java_client.android.AndroidDriver;
 import selenium.Utils.Constant;
 import selenium.Utils.ReadExcel;
 
